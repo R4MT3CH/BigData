d22a645ce40bfedd3890d2bf4e18e798578fb39f
Ramin Roufeh <raminroufeh@gmail.com>
2024-07-24 16:09:29 -0700

    BigData Assignemnt - SQL commit2

---
+++ HackerRank/Code/HR-Making_Anagram.py
@@ -0,0 +1,15 @@
+# Given two strings,  and , that may or may not be of the same length, determine the minimum number of character deletions required to make  and  anagrams. Any characters can be deleted from either of the strings.
+
+# Example
+
+
+# Delete  from  and  from  so that the remaining strings are  and  which are anagrams. This takes  character deletions.
+
+# Function Description
+
+# Complete the makeAnagram function in the editor below.
+
+# makeAnagram has the following parameter(s):
+
+# string a: a string
+# string b: another string
---
+++ HackerRank/Code/HR-Two_String.py
@@ -0,0 +1,37 @@
+# Given two strings, determine if they share a common substring. A substring may be as small as one character.
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'twoStrings' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s1
+#  2. STRING s2
+#
+
+def twoStrings(s1, s2):
+    # Write your code here
+    
+    # set of string s1 AND set of s2
+    if set(s1) & set(s2):
+        return "YES"
+    else:
+        return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
